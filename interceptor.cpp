9c04459377534e5a028f6d7723c691f61fc11be3
Mateusz Szychowski (Muttley) <mateusz@szychowski.it>
2020-04-02 00:09:10 +0200

    fun

--- interceptor.cpp
+++ interceptor.cpp
@@ -2,6 +2,9 @@
 #include <syscall.h>
 #include <errno.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <fmt/format.h>
 
 extern "C" int
@@ -11,38 +14,38 @@ hook(long syscall_number,
 			long arg4, long arg5,
 			long *result)
 {
-	if (syscall_number == SYS_getdents) {
-		/*
-		 * Prevent the application from
-		 * using the getdents syscall. From
-		 * the point of view of the calling
-		 * process, it is as if the kernel
-		 * would return the ENOTSUP error
-		 * code from the syscall.
-		 */
-		return 1;
-		*result = -ENOTSUP;
-		return 0;
-    }
-    else if (syscall_number == SYS_openat)
+    if (syscall_number == SYS_openat)
     {
-        fmt::print("hei\n");
+        auto const path = reinterpret_cast<char*>(arg1);
+        if (path != nullptr)
+          fmt::print(stderr, "openat {} {} {} {} {} {}\n", arg0, path, arg2, arg3, arg4, arg5);
+        else
+          fmt::print(stderr, "openat {} {} {} {} {} {}\n", arg0, arg1, arg2, arg3, arg4, arg5);
         return 1;
     }
-    else{
-		/*
+    else if (syscall_number == SYS_open)
+    {
+        auto const path = reinterpret_cast<char*>(arg1);
+        if (path != nullptr)
+          fmt::print(stderr, "open {} {} {} {} {} {}\n", arg0, path, arg2, arg3, arg4, arg5);
+        else
+          fmt::print(stderr, "open {} {} {} {} {} {}\n", arg0, arg1, arg2, arg3, arg4, arg5);
+        return 1;
+    }
+    else {
+        /*
 		 * Ignore any other syscalls
 		 * i.e.: pass them on to the kernel
 		 * as would normally happen.
 		 */
 		return 1;
-	}
+    }
 }
 
 extern "C" __attribute__((constructor)) void
 init(void)
 {
-    fmt::print("hooking up\n");
+    //fmt::print("hooking up\n");
 	// Set up the callback function
 	intercept_hook_point = hook;
 }
